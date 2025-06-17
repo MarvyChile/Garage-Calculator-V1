278a552efffc360d891770374a1aec1b5770c9cd
MarvyChile <contacto@marvy.cl>
2025-06-17 11:32:31 -0400

    Update garage_tiles_manual.py

--- garage_tiles_manual.py
+++ garage_tiles_manual.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import math
 
 st.set_page_config(layout="centered")
-st.title("Garage Tile Designer Manual v3.14.4")
+st.title("Piso Garage - Calculadora V1")
 
 # 1. Unidad de medida y entradas
 unidad = st.selectbox("Selecciona la unidad de medida", ["metros", "centímetros"], key="unidad")
